9e6de5db172aed53da4a0d6aba09cb24be8fb34d
Leilani Rants <rantslm@users.noreply.github.com>
2026-03-10 14:29:14 -0400

    add contacts page UI w selectable contact list

--- capstone-frontend/src/App.jsx
+++ capstone-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import ApplicationsPage from './pages/ApplicationsPage';
 import DashboardPage from './pages/DashboardPage';
+import ContactsPage from './pages/ContactsPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<AuthPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/applications" element={<ApplicationsPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
