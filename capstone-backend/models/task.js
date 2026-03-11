3fdeec9abefa97672e759bed470adef1b8f767d0
Leilani Rants <rantslm@users.noreply.github.com>
2026-03-10 23:17:48 -0400

    Update Task model to allow nullable application_id

--- capstone-backend/models/task.js
+++ capstone-backend/models/task.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       application_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
       },
       title: {
         type: DataTypes.STRING,
